7e2be8814e4b6754af5da736509eaa2b3f5a965c
stacyyy <319499970@qq.com>
2020-12-21 21:06:38 +0800

    Transfer number to string before using methods of string.

--- calculator/src/calculator.js
+++ calculator/src/calculator.js
@@ -9,10 +9,7 @@ var reset = function(){
 }
 
 var fitSize = function(n){
-	// var tmp = typeof(n) == "number" ? n: Number(n);
-	// if (view.indexOf(".") != -1) {
-	// 	n--;
-	// }
+// 除小数点外自适应支持9个数字
 	$(".display").css("font-size",45 - (n - 6) * 5 + "px");
 	$(".display").css("padding-top",40 + (n - 6) * 5 + "px");
 
@@ -24,6 +21,7 @@ var show = function () {
 	$(".display").text(view);
 }
 
+// 点击数字按钮瞬间改变背景色
 $(".number").mousedown(function(){
 	$(this).addClass("clickNumber");
 }).mouseup(function(){
@@ -45,16 +43,22 @@ $(".number").click(function(){
 		reset();
 	}
 	var noPoint = view.length;
-	if (view.indexOf(".") != -1) {
+	if (this.innerText == "." || view.indexOf(".") != -1) {
 		noPoint--;
 	}
 	if (noPoint < 9 ){	
 		var number = this.innerText;
+		//小数点只有一个
 		if (number == "." && view.indexOf(".") != -1) {
 			number = "";
-		}
-		if (Number(view) == 0 && Number(number) == 0 && view.indexOf(".") == -1) {
-			view = number;
+		}		
+		if (Number(view) == 0 && view.indexOf(".") == -1) {
+			//小数点前补0，其他时候0不可作为首位
+			if (number == ".") {
+				view = "0" + number;
+			}else {
+				view = number;
+			}
 		}else {
 			view += number;	
 			noPoint++;	    
@@ -74,24 +78,30 @@ $(".ac").click(function(){
 });
 
 $(".delete").click(function(){
-	if (view){
-		view = view.substring(0,view.length-1);
-	 	show();	
-	}
+	// 处理当view为0时，substring方法报错
+	view = view.substring(0,view.length-1);
+	show();	
 
 });
 
 $(".per").click(function(){
-    view = Number(view) / 100;
-    show();
+	view = Number(view) / 100  + "";
+	if (view == "0") {
+		view = "";
+		$(".display").text("0");
+	}else {
+		show();
+	}  
 });
 
 $(".operator").click(function(){
+	// 运算符只有一个被改变为选中样式
 	if($(".operator").hasClass("clickOperator")){
 		$(".operator").removeClass("clickOperator");
 		$(".divide").children().attr('fill','#FFF');
 	}
 	$(this).addClass("clickOperator");
+	// 特殊处理除号svg样式变更
 	if(this.name == "divide"){
 		$(".divide").children().attr('fill','#FF8800');
 	}
